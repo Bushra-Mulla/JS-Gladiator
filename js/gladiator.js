c521add636ec4d87caa026f05026a77cd0864bae
bushra-mulla <bushra.1416@hotmail.com>
2020-12-29 21:47:50 +0300

    Bonus for gladiator

--- js/gladiator.js
+++ js/gladiator.js
@@ -3,16 +3,25 @@ export { Gladiator };
 class Gladiator {
   constructor(name, weapon) {
     this.name = name;
-    this.weapon = weapon; //a weapon (one of Spear, Club, Trident)
-    //  this.weapon = this.checkWeapon(weapon);
+    //   this.weapon = weapon; //a weapon (one of Spear, Club, Trident)
+    this.weapon = this.checkWeapon(weapon);
   }
   //Bonus
-  checkWeapon(weapon) {}
+  checkWeapon(weapon) {
+    let arr = ["Spear", "Club", "Trident"];
+    let result = "";
+    arr.forEach((arrWeapon) => {
+      if (weapon !== arrWeapon) {
+        result = "error";
+      } else {
+        this.weapon = weapon;
+        result = weapon;
+      }
+    });
+    return result;
+  }
 }
 
 //Make it so that you cannot assign a Gladiator an
 //invalid weap on(i.e., anything aside from Spear, Club or
 //Trident.That means running code like new Gladiator("Jesse", "Taco") would throw an error
-
-//test bonus
-//console.log(max.weapon); // "Trident"
--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const max = new Gladiator("Maximus", "Trident");
 console.log(max.name); // "Maximus"
 console.log(max.weapon); // "Trident"
 //Bonus
-//.................
+//................. Done
 const jesse = new Gladiator("Jesse", "Taco");
 console.log(jesse.name);
 console.log(jesse.weapon);
@@ -15,35 +15,35 @@ console.log(jesse.weapon);
 const colosseum = new Arena("Colosseum");
 console.log(colosseum.name); // => Colosseum
 //.................
-const colosseum = new Arena("megalopolis");
-console.log(colosseum.name); // => Megalopolis
+//const colosseum = new Arena("megalopolis");
+//console.log(colosseum.name); // => Megalopolis
 //.................
-const colosseum = new Arena("Colosseum");
-console.log(colosseum.gladiators); // => []
+//const colosseum = new Arena("Colosseum");
+//console.log(colosseum.gladiators); // => []
 //.................
-const max = new Gladiator("Maximus", "Trident");
-const colosseum = new Arena("Colosseum");
-colosseum.addGladiator(max);
-console.log(colosseum.gladiators); // => [Gladiator]
+//const max = new Gladiator("Maximus", "Trident");
+//const colosseum = new Arena("Colosseum");
+//colosseum.addGladiator(max);
+//console.log(colosseum.gladiators); // => [Gladiator]
 //.................
-const max = new Gladiator("Maximus", "Trident");
-const titus = new Gladiator("Titus", "Sword");
-const andronicus = new Gladiator("Andronicus", "Sword");
-const colosseum = new Arena("Colosseum");
+// const max = new Gladiator("Maximus", "Trident");
+// const titus = new Gladiator("Titus", "Sword");
+// const andronicus = new Gladiator("Andronicus", "Sword");
+// const colosseum = new Arena("Colosseum");
 
-colosseum.addGladiator(max);
-colosseum.addGladiator(titus);
-colosseum.addGladiator(andronicus);
+// colosseum.addGladiator(max);
+// colosseum.addGladiator(titus);
+// colosseum.addGladiator(andronicus);
 
-console.log(colosseum.gladiators.length); // => 2
+// console.log(colosseum.gladiators.length); // => 2
 //.................
-const max = new Gladiator("Maximus", "Trident");
-const titus = new Gladiator("Titus", "Spear");
-const colosseum = new Arena("Colosseum");
+// const max = new Gladiator("Maximus", "Trident");
+// const titus = new Gladiator("Titus", "Spear");
+// const colosseum = new Arena("Colosseum");
 
-colosseum.addGladiator(max);
-colosseum.addGladiator(titus);
-colosseum.fight();
+// colosseum.addGladiator(max);
+// colosseum.addGladiator(titus);
+// colosseum.fight();
 
-console.log(colosseum.gladiators); // => [max]
+// console.log(colosseum.gladiators); // => [max]
 //.................
