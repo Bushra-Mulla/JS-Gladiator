251af10f87b2c95fc08765fff53d24ca06c94814
bushra-mulla <bushra.1416@hotmail.com>
2021-01-02 16:24:31 +0300

    ...

--- js/arena.js
+++ js/arena.js
@@ -10,34 +10,71 @@ class Arena {
   }
   addGladiator(gladiators) {
     if (this.gladiators.length < 2) {
-      if (gladiators.weapon == "error") {
+      if (gladiators.weapon === "error") {
         return;
       } else {
         return this.gladiators.push(gladiators);
       }
     }
   }
-
   fight() {
+    let g_1 = this.gladiators[0].weapon;
+    let g_2 = this.gladiators[1].weapon;
+
     if (this.gladiators.length === 2) {
-      //remove the loser
-      for (const kay in this.gladiators) {
-        if (this.gladiators[0].weapon === this.gladiators[1].weapon) {
-          this.gladiators.length = 0;
-        } else if (this.gladiators[kay].weapon === "Spear" && "Trident") {
-          this.gladiators.splice(this.gladiators[kay].weapon === "Spear", +1);
-        } else if (this.gladiators[kay].weapon === "Spear" && "Club") {
-          this.gladiators.splice(this.gladiators[kay].weapon === "Club", +1);
-        } else if (this.gladiators[kay].weapon === "Club" && "Trident") {
-          this.gladiators.splice(this.gladiators[kay].weapon === "Trident", +1);
-        }
+      if (g_1 === g_2) {
+        this.gladiators.length = 0;
+      }
+      if (g_1 === "Trident" && g_2 === "Spear") {
+        this.gladiators.splice(1, 1);
+      } else if (g_1 === "Spear" && g_2 === "Trident") {
+        this.gladiators.splice(0, 1);
+      }
+
+      if (g_1 === "Trident" && g_2 === "Club") {
+        this.gladiators.splice(0, 1);
+      } else if (g_1 === "Club" && g_2 === "Trident") {
+        this.gladiators.splice(1, 1);
+      }
+
+      if (g_1 === "Spear" && g_2 === "Club") {
+        this.gladiators.splice(1, 1);
+      } else if (g_1 === "Club" && g_2 === "Spear") {
+        this.gladiators.splice(0, 1);
       }
     } else {
-      return "There are no enough gladiators";
+      console.log("There are no enough gladiators");
     }
   }
 
+  //Bonus
   removeGladiator(names) {
-    this.gladiators.splice(this.gladiators.name == names, +1);
+    this.gladiators.filter((gladiator) => {
+      const index = gladiator === names;
+      this.gladiators.splice(index, 1);
+    });
   }
+
+  winerMax() {
+    //if the gladiator named "Maximus" is in the fight, he wins.
+    // if (this.gladiators.length === 2) {
+    //   console.log("Maximus is win");
+    //   this.gladiators.filter((gladiator) => {
+    //     gladiator.name === "Maximus";
+    //     console.log("Maximus is win");
+    //   });
+    // }
+  }
+  entertained() {
+    //The crowd is only entertained if Maximus is in the arena.
+  }
+  userVotes(vote, loser) {
+    // if (vote == true) {
+    //   this.gladiators.splice(this.gladiators.name == names, +1);
+    // }
+    //If user votes down, the losing gladiator is removed
+    //If the user votes up, the gladiator stays in the arena and his opponent is removed.
+    //call remove method
+  }
+
 }
--- js/index.js
+++ js/index.js
@@ -3,17 +3,18 @@ import { Arena } from "./arena.js";
 
 const max = new Gladiator("Maximus", "Trident");
 const titus = new Gladiator("Titus", "Spear");
-const andronicus = new Gladiator("Andronicus", "Spear");
 
+const andronicus = new Gladiator("Andronicus", "Spear");
 const colosseum = new Arena("Colosseum");
 
 colosseum.addGladiator(max);
 colosseum.addGladiator(titus);
 colosseum.addGladiator(andronicus);
 colosseum.fight();
-
 console.log(colosseum.gladiators); // => [max]
 
 //.................
-colosseum.removeGladiator("Maximus");
+//colosseum.removeGladiator("Maximus");
+colosseum.winerMax();
+
 console.log(colosseum.gladiators); // => []
